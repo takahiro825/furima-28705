5ceb940edfa7116950cd5cd91bfb514b6d09093c
510takahiro825 <spayair@icloud.com>
2020-09-07 22:12:38 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
-
+  resources :items
+  get 'items', to: 'items#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
