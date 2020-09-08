38d54be5b8d25e0a1f8601243e729f66f05dd762
510takahiro825 <spayair@icloud.com>
2020-09-08 19:18:40 +0900

    修正

--- app/forms/item_image.rb
+++ app/forms/item_image.rb
@@ -1,6 +1,6 @@
-class ItemImage
+# class ItemImage
 
-  include ActiveModel::ActiveModel
-  attr_accessor :image,:name, :explanation, :category_id, :status_id,:shipping_charges_id, :shipping_region_id, :days_until_shipping_id,:selling_pice
+#   include ActiveModel::ActiveModel
+#   attr_accessor :image,:name, :explanation, :category_id, :status_id,:shipping_charges_id, :shipping_region_id, :days_until_shipping_id,:selling_pice
 
-end
+# end
