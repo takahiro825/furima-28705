bd2f20fc791bd0926100890a17c0dd4e750278eb
510takahiro825 <spayair@icloud.com>
2020-09-08 19:56:43 +0900

    Delete item_image.rb

--- app/forms/item_image.rb
+++
@@ -1,6 +0,0 @@
-# class ItemImage
-
-#   include ActiveModel::ActiveModel
-#   attr_accessor :image,:name, :explanation, :category_id, :status_id,:shipping_charges_id, :shipping_region_id, :days_until_shipping_id,:selling_pice
-
-# end
