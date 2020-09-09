8c9d415e30e4e4e7a7cd87d375dabc47fe5f898a
510takahiro825 <spayair@icloud.com>
2020-09-09 18:24:14 +0900

    商品詳細機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,12 @@ class ItemsController < ApplicationController
     end
   end
 
+    def show
+      @items = Item.find(params[:id])
+
+    end
+  
+
   private
   def item_params
     params.require(:item).permit(:image,:name, :explanation, :category_id, :status_id,:shipping_charges_id, :shipping_region_id, :days_until_shipping_id,:selling_price ) .merge(user_id: current_user.id)
