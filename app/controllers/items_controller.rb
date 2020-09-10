ac9d96afdc31a85a54997edfb33015533b757504
510takahiro825 <spayair@icloud.com>
2020-09-10 19:02:56 +0900

    コントローラー内のコードをまとめ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show,:edit, :update]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -17,15 +18,12 @@ class ItemsController < ApplicationController
   end
 
     def show
-      @item = Item.find(params[:id])
     end
   
     def edit
-      @item = Item.find(params[:id])
     end
 
     def update
-      @item = Item.find(params[:id])
       if @item.user_id == current_user.id
          if@item.update(item_params)
            redirect_to item_path(@item.id)
@@ -40,5 +38,9 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image,:name, :explanation, :category_id, :status_id,:shipping_charges_id, :shipping_region_id, :days_until_shipping_id,:selling_price ) .merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
