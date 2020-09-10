8f6f244f1cb44de03fe8edc651a1e6d6f3be9fc5
510takahiro825 <spayair@icloud.com>
2020-09-10 18:06:38 +0900

    出品商品の編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,22 @@ class ItemsController < ApplicationController
       @item = Item.find(params[:id])
     end
   
+    def edit
+      @item = Item.find(params[:id])
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      if @item.user_id == current_user.id
+         if@item.update(item_params)
+           redirect_to item_path(@item.id)
+         else
+          render :edit
+         end
+      else
+        render :edit
+      end
+    end
 
   private
   def item_params
