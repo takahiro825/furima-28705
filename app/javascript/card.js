fac4ada5ef726bcad9a50d4138cf016c5954e774
510takahiro825 <spayair@icloud.com>
2020-09-14 18:51:10 +0900

    JS　console.logの削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,10 +13,7 @@ const pay = () => {
       exp_month: formData.get("exp_month"),
       exp_year: `20${formData.get("exp_year")}`,
     };
-    console.log(card)
     Payjp.createToken(card, (status, response) => {
-      console.log(response.errors)
-      console.log(status)
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
